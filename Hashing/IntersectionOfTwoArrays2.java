21e0a79fe458f3f0968b419bc7f260b683aa37c0
HunterChen <1480811935cyt@gmail.com>
2023-05-07 22:28:55 +0800

    IntersectionOfTwoArrays2

---
+++ Hashing/IntersectionOfTwoArrays2.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class IntersectionOfTwoArrays2 {
+    public int[] intersect(int[] nums1, int[] nums2) {
+        int[] have = new int[1001];
+        for (int i = 0; i < nums2.length; i++) {
+            have[nums2[i]]++;
+        }
+        List<Integer> list = new ArrayList<>();
+        for (int i = 0; i < nums1.length; i++) {
+            if (have[nums1[i]] != 0) {
+                list.add(nums1[i]);
+                have[nums1[i]]--;
+            }
+        }
+        return list.stream().mapToInt(i -> i).toArray();
+    }
+}
